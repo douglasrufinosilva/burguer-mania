4b727129275e9865d20c7bc9e73ee4aba382393f
Douglas Rufino <douglasrufinosilva@gmail.com>
2025-01-14 22:03:41 -0300

    migration criada

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'Drs.95857002Md',
+  database: 'burguer-mania',
+  define: {
+    timespamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
