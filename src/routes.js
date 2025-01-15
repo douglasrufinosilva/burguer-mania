89403390fc6c5bad87ffa37023c1ab87183b12e8
Douglas Rufino <douglasrufinosilva@gmail.com>
2025-01-15 13:50:11 -0300

    conexão com banco de dados, e model user criado

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express'
+import { v4 } from 'uuid'
+import User from './app/models/User'
 
 const routes = Router()
 
-routes.get('/', (req, res) => {
-  res.status(200).json({ messade: 'teste' })
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Douglas',
+    email: 'doug@mail.com',
+    password_hash: 'asjldj84lajd',
+  })
+
+  res.status(200).json(user)
 })
 
 export default routes
