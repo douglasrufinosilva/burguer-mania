20d853edbcf484693fd9363ebf284f06a5f1a98d
Douglas Rufino <douglasrufinosilva@gmail.com>
2025-01-16 12:33:23 -0300

    controller de login, rota e verificação senha criados

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express'
 import UserController from './app/controllers/UserController'
+import SessionController from './app/controllers/SessionController'
 
 const userController = new UserController()
+const sessionController = new SessionController()
 
 const routes = Router()
 
+// rota de usuários
 routes.post('/users', userController.store)
+
+routes.post('/session', sessionController.store)
 
 export default routes
