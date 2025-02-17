d98614e84230780a919d938e040fdba0ff626bd8
Douglas Rufino <douglasrufinosilva@gmail.com>
2025-02-17 19:46:01 -0300

    instalação e configuração do jwt na session

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,4 +1,6 @@
 import * as Yup from 'yup'
+import jwt from 'jsonwebtoken'
+import authConfig from '../../config/auth'
 import User from '../models/User'
 
 class SessionController {
@@ -37,6 +39,9 @@ class SessionController {
       email: user.email,
       name: user.name,
       admin: user.admin,
+      token: jwt.sign({ id: user.id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
     })
   }
 }
